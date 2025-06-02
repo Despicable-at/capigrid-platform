5150ec2c621965016139a96a06560e198baf38be
Despicable-at <agyemangtakyi2021@gmail.com>
2025-06-02 15:00:35 +0000

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,6 @@
 import type { Express } from "express";
-import { createServer, type Server } from "http";+import { storage } from "./storage.js";
+import { createServer, type Server } from "http";
+import { storage } from "./storage.js";
 import { setupAuth, isAuthenticated } from "./replitAuth.js";
 import bcrypt from "bcryptjs";
 import speakeasy from "speakeasy";
